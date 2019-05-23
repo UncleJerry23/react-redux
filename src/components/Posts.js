d2ccb330f8eefaddf3f3b5e707f85d6bbe099ef8
UncleJerry <jared.myhrberg@gmail.com>
2019-05-23 14:05:47 -0700

    removed logs

--- src/components/Posts.js
+++ src/components/Posts.js
@@ -3,16 +3,12 @@ import PropTypes from 'prop-types';
 import Post from './Post';
 
 function Posts({ postArr }) {
-  console.log('postArr', postArr);
-
   const postList = postArr.map(post => (
 
     <li key={post.id}>
       <Post post={post} />
     </li>
   ));
-
-  console.log('postList', postList);
 
   return (
     <ul>
