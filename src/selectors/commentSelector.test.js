16cb39fa296cc8bbee366027489070831960cb33
UncleJerry <jared.myhrberg@gmail.com>
2019-05-23 10:58:32 -0700

    comment selector test passing

---
+++ src/selectors/commentSelector.test.js
@@ -0,0 +1,30 @@
+import { getComments } from './commentSelectors';
+
+describe('comment selector', () => {
+  it('gets that there comment', () => {
+    const state = {
+      posts: [
+        { title: 'mypost', postBody: 'bla bla bla?' },
+        { title: 'yourpost', postBody: 'yee haww?' }
+      ],
+      comments: [
+        { 
+          postTitle: 'mypost', 
+          commentTitle: 'myComment', 
+          comment: 'great post' 
+        },
+        { 
+          postTitle: 'mypost', 
+          commentTitle: 'your comment', 
+          comment: 'bad post' 
+        }
+      ]
+    };
+
+    expect(getComments(state, 'myComment')).toEqual({ 
+      postTitle: 'mypost', 
+      commentTitle: 'myComment', 
+      comment: 'great post' 
+    });
+  });
+});
--- src/selectors/commentSelectors.js
+++ src/selectors/commentSelectors.js
@@ -1,1 +1,1 @@
-export const getComments = (state, title) => state.comments[title] || [];
+export const getComments = (state, title) => state.comments.find(comment => comment.commentTitle === title) || [];
