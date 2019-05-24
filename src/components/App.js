f05ad820afb7ae379ce4473f80c8e589ac0bc6ce
UncleJerry <jared.myhrberg@gmail.com>
2019-05-24 10:29:19 -0700

    added header link to home page

--- src/components/App.js
+++ src/components/App.js
@@ -2,7 +2,8 @@ import React from 'react';
 import {
   BrowserRouter as Router,
   Switch,
-  Route
+  Route,
+  Link
 } from 'react-router-dom';
 import PostById from '../containers/posts/PostById';
 import Home from './Home';
@@ -10,9 +11,10 @@ import Home from './Home';
 export default function App() {
   return (
     <Router>
+      <Link to="/" >Home</Link>
       <Switch>
-        <Route path="/" component={Home} />
         <Route path="/post/:id" component={PostById} />
+        <Route path="/" component={Home} />
       </Switch>
     </Router>
   );
